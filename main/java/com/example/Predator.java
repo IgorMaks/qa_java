e392b4304df85d7a5703c14abf5609857cbdea36
IgorMaks <orkmedv@gmail.com>
2024-06-16 10:04:33 +0300

    Update Predator.java
    Убраны правки

--- main/java/com/example/Predator.java
+++ main/java/com/example/Predator.java
@@ -6,8 +6,4 @@ public interface Predator {
 
     List<String> eatMeat() throws Exception;
 
-    int getKittens() throws Exception;
-
-    List<String> getFood(String string) throws Exception;
-
 }
