ab448759f88b9eb9abb7f4e50d126c34b7c86f4c
IgorMaks <orkmedv@gmail.com>
2024-06-16 10:06:18 +0300

    Update CatTest.java
    Переписаны тесты

--- test/java/com/example/CatTest.java
+++ test/java/com/example/CatTest.java
@@ -1,36 +1,36 @@
 package com.example;
 
-import junit.framework.TestCase;
-import org.junit.Before;
+import com.example.Cat;
+import com.example.Feline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(MockitoJUnitRunner.class)
-public class CatTest extends TestCase {
-    private Cat cat;
+public class CatTest {
 
     @Mock
     Feline feline;
 
-    @Before
-    public void setUp() {
-        cat = new Cat(feline);
-    }
-
-
     @Test
-    public void testGetSound() {
-        String actual = cat.getSound();
-
-        assertEquals("Мяу", actual);
+    public void getSoundExpectsSound() {
+        Cat cat = new Cat(feline);
+        String sound = cat.getSound();
+        assertEquals("Мяу", sound);
     }
 
     @Test
-    public void testGetFood() throws Exception {
-        cat.getFood();
-        Mockito.verify(feline).eatMeat();
+    public void getFoodExpectsFood() throws Exception {
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> food = cat.getFood();
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), food);
     }
+
 }
